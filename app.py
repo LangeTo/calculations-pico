39bc44e6b5b968f29bd564b20361a95b5cfd2663
LangeTo <toblange@t-online.de>
2024-07-16 16:39:59 +0200

    Starting to understand to logic behind this.

--- app.py
+++ app.py
@@ -5,12 +5,6 @@ from shiny.types import FileInfo
 
 app_ui = ui.page_fluid(
     ui.input_file("file1", "Choose CSV File", accept=[".csv"], multiple=False),
-    # ui.input_checkbox_group(
-    #     "stats",
-    #     "Summary Stats",
-    #     choices=["Row Count", "Column Count", "Column Names"],
-    #     selected=["Row Count", "Column Count", "Column Names"],
-    # ),
     ui.output_table("summary"),
 )
 
@@ -21,9 +15,8 @@ def server(input: Inputs, output: Outputs, session: Session):
         file: list[FileInfo] | None = input.file1()
         if file is None:
             return pd.DataFrame()
-        return pd.read_csv(  # pyright: ignore[reportUnknownMemberType]
-            file[0]["datapath"], sep=",", skiprows=1
-        )
+        # skiprows=1 is at least necessary for the test MO file from QIAcuity
+        return pd.read_csv(file[0]["datapath"], sep=",", skiprows=1)
 
     @render.table
     def summary():
@@ -32,24 +25,6 @@ def server(input: Inputs, output: Outputs, session: Session):
         if df.empty:
             return pd.DataFrame()
 
-        # # Get the row count, column count, and column names of the DataFrame
-        # row_count = df.shape[0]
-        # column_count = df.shape[1]
-        # names = df.columns.tolist()
-        # column_names = ", ".join(str(name) for name in names)
-
-        # # Create a new DataFrame to display the information
-        # info_df = pd.DataFrame(
-        #     {
-        #         "Row Count": [row_count],
-        #         "Column Count": [column_count],
-        #         "Column Names": [column_names],
-        #     }
-        # )
-
-        # # input.stats() is a list of strings; subset the columns based on the selected
-        # # checkboxes
-        # return info_df.loc[:, input.stats()]
         return df
 
 
