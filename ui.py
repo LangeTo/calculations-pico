a82a602acddf403205db0e433ac6f1f54350191a
LangeTo <toblange@t-online.de>
2024-07-22 17:26:09 +0200

    Working on the ui and the documentation.

--- ui.py
+++ ui.py
@@ -5,15 +5,23 @@ app_ui = ui.page_fluid(
     ui.card(
         ui.h1("Implementation of dDPCS model for the calculation of couplexes"),
         ui.p(
-            "This application is an implementation of the calculation of couplexes introduced in my PhD thesis (see below). At the moment, it uses the MultipleOccupancy file from the QIAcuity Software Suite 2.5.0.1. It then "
-            "This application allows you to upload a MultipleOccupancy file "
-            "from the QIAcuity Software Suite 2.5.0.1, process it, and download "
-            "the processed data. Additionally, you can visualize the results in "
-            "a plot."
+            "This application is an implementation of the calculation of couplexes introduced in my PhD thesis (see below). At the moment, it uses the MultipleOccupancy file from the QIAcuity Software Suite 2.5.0.1. Here is what the app does:"
+        ),
+        ui.tags.li(
+            "It checks the number of antibodies used in the PICO assay and calculates their possible combinations (colorpairs)."
+        ),
+        ui.tags.li(
+            "For all combinations, it calculates the number of partitions in the single and double positive clusters as well as the number of negative partitions."
+        ),
+        ui.tags.li(
+            "Then, it calculates the number of couplexes using the dDPCS model."
+        ),
+        ui.tags.li(
+            "Finally, it plots the number of couplexes for all antibody combinations (colorpairs) and provides a .csv file to download."
         ),
         ui.p(
-            "The source code is available from",
-            ui.a("official documentation", href="https://github.com/LangeTo"),
+            "The source code is available from ",
+            ui.a("GitHub", href="https://github.com/LangeTo"),
             ".",
         ),
     ),
@@ -30,8 +38,7 @@ app_ui = ui.page_fluid(
                 ui.download_button("download_data", "Download processed data"),
             ),
             ui.card(
-                ui.h3("PDF Display"),
-                ui.p("Below is a PDF display starting from page 2:"),
+                ui.h3("PhD thesis Tobias Hundertmark"),
                 ui.tags.object(
                     data="/www/PhD_thesis_Hundertmark.pdf#page=67",
                     type="application/pdf",
